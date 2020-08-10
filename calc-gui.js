0fedd5bff263533bf9079c8a1b8ffb2545098374
Cyaegha96 <zazae51@gmail.com>
2020-08-10 19:47:46 +0900

    calculater gui -#1
    button을 이용한 계산기 버튼 구현.
    버튼에 이어지는 함수들 구현, event를 통해 배정된 값 가져오기
    자바스크립트 파일은 별도의 파일로 분리

---
+++ calc-gui.js
@@ -0,0 +1,9 @@
+var clickNumbers = function(evenet){
+    console.log("click Numbers");
+    console.log(event.target.innerHTML);
+}
+
+var clickOthers = function(event){
+    console.log("click others");
+    console.log(event.target.innerHTML);
+}
